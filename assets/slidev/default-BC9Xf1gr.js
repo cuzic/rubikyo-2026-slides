3470ce69cf54ca4a9db4c67bf0385c98055a792e
Tomoya Kawanishi <tomoya.kaw@gmail.com>
2026-02-26 20:11:24 -0600

    Updates

---
+++ assets/slidev/default-BC9Xf1gr.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-Ci6EBCGa.js";import{_ as r}from"../index-BD1m3Tpq.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
